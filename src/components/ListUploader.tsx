53b2a6f3d94df75ec6bd8f10dc11654827fffc9b
soobing <qls0147@naver.com>
2022-02-05 02:05:43 +0900

    feat: input upload

--- src/components/ListUploader.tsx
+++ src/components/ListUploader.tsx
@@ -1,9 +1,29 @@
+import { useRef } from "react";
 import "./ListUploader.css";
 
 export default function ListUploader() {
+  const inputEl = useRef<HTMLInputElement>(null);
+  const onClickUpload = () => {
+    inputEl?.current?.click();
+  };
+
+  const onChangeFile = (e: React.ChangeEvent<HTMLInputElement>) => {
+    Array.from(e.target.files ?? []).map((file) =>
+      console.log(URL.createObjectURL(file))
+    );
+  };
+
   return (
     <div className="list-uploader__wrapper">
-      <button className="list-uploader__button">사진 첨부하기</button>
+      <input
+        ref={inputEl}
+        type="file"
+        style={{ display: "none" }}
+        onChange={onChangeFile}
+      />
+      <button className="list-uploader__button" onClick={onClickUpload}>
+        사진 첨부하기
+      </button>
       <ul className="list-uploader__cards">
         <li className="list-uploader__card">
           <div className="list-uploader__thumbnail">
--- src/components/ThumbnailUploader.tsx
+++ src/components/ThumbnailUploader.tsx
@@ -1,11 +1,34 @@
+import { useRef } from "react";
 import "./ThumbnailUploader.css";
 
 export default function ThumbnailUploader() {
+  const inputEl = useRef<HTMLInputElement>(null);
+  const onClickUpload = () => {
+    inputEl?.current?.click();
+  };
+
+  const onChangeFile = (e: React.ChangeEvent<HTMLInputElement>) => {
+    Array.from(e.target.files ?? []).map((file) =>
+      console.log(URL.createObjectURL(file))
+    );
+  };
+
   return (
     <div className="thumbnail-uploader__wrapper">
+      <input
+        ref={inputEl}
+        type="file"
+        style={{ display: "none" }}
+        onChange={onChangeFile}
+      />
       <ul className="thumbnail-uploader__cards">
         <li>
-          <button className="thumbnail-uploader__button">+</button>
+          <button
+            className="thumbnail-uploader__button"
+            onClick={onClickUpload}
+          >
+            +
+          </button>
         </li>
         <li className="thumbnail-uploader__card">
           <img
